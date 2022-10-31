21b8687865222035b0a7071cb59a7395d3b6fda5
DiasErick <116904851+DiasErick@users.noreply.github.com>
2022-10-31 14:09:17 -0300

    Test with coordinating of Fredericton City

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
 from weather import getCurrentWeather
 
 #Just a test to make sure qye can reach the endpoint
-print( getCurrentWeather("9c2e93f176d25559234a3d43712a506e", 52.51743140042971, 13.406459286106932) )
+print( getCurrentWeather("9c2e93f176d25559234a3d43712a506e", 45.995321594837755, -66.65457929809905) )
